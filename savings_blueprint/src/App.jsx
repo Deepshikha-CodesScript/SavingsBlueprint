5f92a0ddc1d4385e95f77031e1d50f8558996968
Deepshikha <deepshikha5hrivastav4@gmail.com>
2026-06-10 11:23:00 +0530

    updated code

--- savings_blueprint/src/App.jsx
+++ savings_blueprint/src/App.jsx
@@ -76,7 +76,7 @@ function App() {
 
                   <Route
                     path="/savings"
-                    element={<Savings />}
+                    element={<Savings />}     
                   />
 
                   <Route
