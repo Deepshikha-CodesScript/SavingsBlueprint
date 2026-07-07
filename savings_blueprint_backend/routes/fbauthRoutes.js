ac1b2a4f9b794c81a75a4f67c1f98081726f8635
Deepshikha <deepshikha5hrivastav4@gmail.com>
2026-07-07 18:56:35 +0530

    Initial commit

--- savings_blueprint_backend/routes/fbauthRoutes.js
+++ savings_blueprint_backend/routes/fbauthRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 router.post(
   "/facebook",
   async (req, res) => {
